a9801d76ec0d1eaeb4624fd374b8148fde194f75
Maxim Bredin <mbredin@bk.ru>
2022-05-22 02:19:07 +0300

    Change practical_work_1.R

--- Practical_works/practical_work_1.R
+++ Practical_works/practical_work_1.R
@@ -1,9 +1,8 @@
 library(nnet)
-library(NeuralNetTools)
-x1<-c(0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1)
-x2<-c(0,0,0,0,1,1,1,1,0,0,0,0,1,1,1,1)
-x3<-c(0,0,1,1,0,0,1,1,0,0,1,1,0,0,1,1)
-x4<-c(0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1)
+x1<-rep(c(0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1))
+x2<-rep(c(0,0,0,0,1,1,1,1,0,0,0,0,1,1,1,1))
+x3<-rep(c(0,0,1,1,0,0,1,1,0,0,1,1,0,0,1,1))
+x4<-rep(c(0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1))
 #матрица значений
 x<-matrix(0,nrow=16,ncol=4)
 for(i in 1:4){
@@ -15,13 +14,14 @@ for(i in 1:4){
 n<-rep(c(0),16)
 for (i in 1:16) {
   n1<- (xor(x1[i],x2[i]))
-  n2<- (xor(x2[i],x3[i]))
-  n3<- (xor(x3[i],x4[i]))
-  n[i]<- !(n1|n2|n3)
+  n2<- (xor(n1,x3[i]))
+  n3<- (xor(n2,x4[i]))
+  n[i]<- !(n3)
 }
 #визуализация результата
 plot(n,type='l',col='red',lwd=7)
 set.seed(35)
-Nnet<-nnet(x,n,size=2,linout=TRUE, rang=0.5)
+Nnet<-nnet(x,n,size=2,linout=TRUE, rang=0.22)
+library(NeuralNetTools)
 plotnet(Nnet)
 predict(Nnet,x)
